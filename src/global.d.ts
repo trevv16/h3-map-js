6e5a9c546a16161f21d8c7da67ff52befc02c6f9
trevv16 <tnjeru16@gmail.com>
2022-05-22 17:38:11 -0400

    removed unused types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,16 +1,4 @@
 export type Coordinates = [number, number];
-export type Polygon = Coordinates[];
-export type MultiPolygon = Polygon[];
-
-export interface IBounds {
-  ne: Coordinates;
-  sw: Coordinates;
-}
-
-export interface ILatLng {
-  lat: number;
-  lng: number;
-}
 
 export type H3Index = string;
 export type H3Set = H3Index[];
